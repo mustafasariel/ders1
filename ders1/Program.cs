192162338b9d300fb10431668fed54fce29b420f
mustafasariel <mustafasariel@gmail.com>
2019-05-26 14:08:09 +0300

    döngü update

--- ders1/Program.cs
+++ ders1/Program.cs
@@ -23,7 +23,8 @@ namespace ders1
 
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i.ToString());
+                Console.WriteLine($" i: {i}");
+                
             }
           
         }
